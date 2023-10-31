283ca23e589e7761dc79f8e4d182959a0869e880
olfamoussaoui <moussaoui.olfa1@gmail.com>
2023-10-31 23:18:03 +0100

    generate the project and configure datasource

---
+++ src/main/java/com/olfa/issue/ticketing/IssueTcketingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.olfa.issue.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTcketingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTcketingAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olfa/issue/ticketing/IssueTcketingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olfa.issue.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTcketingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
